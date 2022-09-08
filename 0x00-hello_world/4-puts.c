5d5e9bd27d3594e0d2919d037061736e90aa27e9
Aryton Koskei <arytonkosgei@gmail.com>
2022-09-08 04:28:24 -0700

    print string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alwats 0 (success)
+ */
+int main(void)
+{
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0);
+}
