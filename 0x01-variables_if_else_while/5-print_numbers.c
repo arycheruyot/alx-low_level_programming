9016bf4eea54ce5cdf47b20af4ff4ea5d03c2976
Aryton Koskei <arytonkosgei@gmail.com>
2022-09-09 05:59:12 -0700

    print numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
