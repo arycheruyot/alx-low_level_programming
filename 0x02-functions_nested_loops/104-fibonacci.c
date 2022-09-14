ea812f950bf6d42d9157028b55d3e970548a672f
Aryton Koskei <arytonkosgei@gmail.com>
2022-09-14 02:17:55 -0700

    find and print the first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,41 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the even-valued terms
- * followed by a new line
- * Return: Always 0 (Success)
+ * main - print fibonacci
+ *
+ * Return: always 0
  */
 int main(void)
 {
-	int i;
-	unsigned long int j, k, next, sum;
+	unsigned long int a, a1, a2, b, b1, b2, c, c1, c2, d, e;
 
-	j = 1;
-	k = 2;
-	sum = 0;
+	a = 1;
+	b = 2;
+	c = a + b;
 
-	for (i = 1; i <= 33; ++i)
+	printf("%lu, ", a);
+	printf("%lu, ", b);
+	for (d = 3; d < 89; d++)
 	{
-		if (j < 4000000 && (j % 2) == 0)
-		{
-			sum = sum + j;
-		}
-		next = j + k;
-		j = k;
-		k = next;
+		printf("%lu, ", c);
+		a = b;
+		b = c;
+		c = a + b;
 	}
-
-	printf("%lu\n", sum);
-
+	b1 = b / 1000000000;
+	b2 = b % 1000000000;
+	c1 = c / 1000000000;
+	c2 = c % 1000000000;
+	for (e = 89; e < 98; e++)
+	{
+		printf("%lu%lu, ", c1, c2);
+		a1 = b1;
+		a2 = b2;
+		b1 = c1;
+		b2 = c2;
+		c1 = a1 + b1 + ((a2 + b2) / 1000000000);
+		c2 = (a2 + b2) % 1000000000;
+	}
+printf("%lu%lu\n", c1, c2);
 	return (0);
 }
