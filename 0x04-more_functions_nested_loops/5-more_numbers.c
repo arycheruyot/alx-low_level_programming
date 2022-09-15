ca24912d36ed2275bb700bfec61bda1c4e25920a
Aryton Koskei <arytonkosgei@gmail.com>
2022-09-15 02:01:21 -0700

    print numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14
+ *
+ * Return: numbers
+ */
+void more_numbers(void)
+{
+	char a, b, c, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c < 10)
+			{
+				b = c;
+			}
+			else
+			{
+				a = c / 10;
+				b = c % 10;
+				_putchar('0' + a);
+			}
+			_putchar('0' + b);
+		}
+		_putchar('\n');
+	}
+}
