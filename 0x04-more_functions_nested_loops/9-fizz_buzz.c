0ba4647c1aaa0b9615792ef67eccf340bcc101b8
Aryton Koskei <arytonkosgei@gmail.com>
2022-09-15 04:32:13 -0700

    print numbers 1 to 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,24 +9,18 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-		{
-			printf("Fizzbuzz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
 		if (i < 100)
-		{
 			printf(" ");
-		}
 	}
 	printf("\n");
 	return (0);
