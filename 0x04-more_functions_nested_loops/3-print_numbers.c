27d8eca68b391b342390ed3e20fd51e3106c0e7e
Aryton Koskei <arytonkosgei@gmail.com>
2022-09-15 01:32:00 -0700

    print numbers between 0 and 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; i++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
