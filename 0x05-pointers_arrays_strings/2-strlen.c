ddd3eb09ea2799f5aea9ecda721bee88b96af7e2
Aryton Koskei <arytonkosgei@gmail.com>
2022-09-19 04:00:13 -0700

    return the lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string to be tested
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
