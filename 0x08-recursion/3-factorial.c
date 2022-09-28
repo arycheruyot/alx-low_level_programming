3887f311407ee8938a9b3e409625deffbdd1ebee
Aryton Koskei <arytonkosgei@gmail.com>
2022-09-28 03:32:49 -0700

    return a factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,18 +4,11 @@
  * @n: integer to be factored
  * Return: integer
  */
-int factorial(int n);
+int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n == 0)
-	{
+	if (n == 0)
 		return (1);
-	}
-	else
-	{
-		return (n * factorial(n - 1));
-	}
+	return (n * factorial(n - 1));
 }
